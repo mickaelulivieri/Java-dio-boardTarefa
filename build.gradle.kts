422f501c83d6a3d5d8b9bc2f0a7351e341232dce
mickaelulivieri <mmickael.carvalho@gmail.com>
2025-08-05 19:24:38 -0300

    config inicial do projeto com as tabelas definidas e a primeira migration

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("org.liquibase:liquibase-core:4.29.1")
+    implementation("mysql:mysql-connector-java:8.0.33")
+    implementation("org.projectlombok:lombok:1.18.34")
+
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/br/com/persistence/Main.java
@@ -0,0 +1,13 @@
+package br.com.persistence;
+
+import br.com.persistence.migration.MigrationStrategy;
+import java.sql.SQLException;
+import static br.com.persistence.config.ConnectionConfig.getConnection;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        try(var connection = getConnection()){
+            new MigrationStrategy(connection).executeMigration();
+        }
+    }
+}

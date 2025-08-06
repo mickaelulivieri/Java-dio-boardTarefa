4640809bd786570f2927e97faabdff47226845b0
mickaelulivieri <mmickael.carvalho@gmail.com>
2025-08-06 14:19:09 -0300

    adicionando a camada service e algumas entitys

--- src/main/resources/db/changelog/migrations/db.changelog-202408211004.sql
+++ src/main/resources/db/changelog/migrations/db.changelog-202408211004.sql
@@ -5,7 +5,6 @@ CREATE TABLE CARDS(
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
-    `order` int NOT NULL,
     board_column_id BIGINT NOT NULL,
     CONSTRAINT boards__boards__cards_column_fk FOREIGN KEY (board_column_id) REFERENCES BOARDS_COLUMNS(id) ON DELETE CASCADE,
 ) ENGINE=InnoDB;
